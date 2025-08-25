739af362c4e9c51c2d2d1f22142d106805720248
Jerome Prinet <jprinet@gradle.com>
2025-08-25 14:16:10 +0200

    Add settings plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,13 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
         maven { url = uri("https://repo.spring.io/milestone") }
         gradlePluginPortal()
     }
+}
+
+plugins {
+    id("io.micronaut.platform.catalog") version "4.3.4-SNAPSHOT"
 }
 
 rootProject.name = "demo"
